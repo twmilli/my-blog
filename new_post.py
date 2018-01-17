23ca58e7def435422494739e89186da439081153
twmilli <twm013@bucknell.edu>
2018-01-16 20:03:33 -0500

    anti growth hacking

--- new_post.py
+++ new_post.py
@@ -4,9 +4,15 @@ import datetime
 def main():
     title = input("Enter the title: ").title()
     tag = input("Enter the tag: ").title()
-    now = datetime.datetime.now()
+    date = input("Enter the publishing date ")
+    if date == "":
+        now = datetime.datetime.now()
+        date = str(now.year) + "-" + pad_zero(now.month) + "-" + pad_zero(now.day)
+    else:
+        date_array = date.split("-")
+        date = str(date_array[2]) + "-" + pad_zero(date_array[0]) + "-" + pad_zero(date_array[1])
+        
     path = "-".join(title.lower().split())
-    date = str(now.year) + "-" + pad_zero(now.month) + "-" + pad_zero(now.day)
     dir_name = date + "-" + path
     os.mkdir("./src/pages/" + dir_name)
     f  = open("./src/pages/" + dir_name + "/index.md", "w+")
@@ -20,6 +26,7 @@ def main():
     f.close()
 
 def pad_zero(n):
+    n = int(n)
     if n < 10:
         return "0" + str(n)
     return str(n)

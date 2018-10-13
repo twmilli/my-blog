736a78ae7982a23effcf555910b067308c400af5
twmilli <twm013@bucknell.edu>
2018-10-13 15:38:48 -0700

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,20 +16,21 @@ const About = () => (
         <p>
                     When I left for college, I had know idea what I wanted to do with my life.
                     I ended up falling in love with the tech industry, learned to code, and landed an internship at <a href="https://www.braintreepayments.com"> Braintree</a>.
+                    Now I work @ <a href="https://www.thumbtack.com/">Thumbtack</a> on the Marketplace team.
         </p>
       </section>
       <section>
         <h3>Writing</h3>
-        <p>In 2016, I published my first piece on <a href="https://medium.com/@twm013">medium</a> documenting my journey of learning to code. The <a href="https://medium.freecodecamp.org/how-doing-something-i-love-landed-me-a-top-tier-tech-internship-fe78d8b74e48">article</a> received over 2.6k claps on medium and was republished by the <a href="http://observer.com/2016/12/how-i-landed-a-top-tier-tech-internship-by-doing-something-i-love/">Observer</a>.
-                    Since then I've fallen in love with writing as a way to think more clearly, connect with others, and learn from mistakes.  In the process, I've picked up over 120k views on medium.
-                    Recently I've started freelance writing for <a href="https://blog.logrocket.com/">Log Rocket</a>.  More on that to come soon :).
+        <p>In 2016, I published my first piece on <a href="https://medium.com/@twm013">medium</a> documenting my journey of learning to code. The <a href="https://medium.freecodecamp.org/how-doing-something-i-love-landed-me-a-top-tier-tech-internship-fe78d8b74e48">article</a> received over 3k claps on medium and was republished by the <a href="http://observer.com/2016/12/how-i-landed-a-top-tier-tech-internship-by-doing-something-i-love/">Observer</a>.
+                    Since then I've fallen in love with writing as a way to think more clearly, connect with others, and learn from mistakes.  In the process, I've picked up nearly 200k views on medium.
+                    Recently I've started freelance writing for <a href="https://programmingwithmosh.com/">Code with Mosh</a>.  More on that to come soon :).
         </p>
       </section>
       <section>
         <h3>School</h3>
         <p>
-                    I'm currrently a senior at Bucknell University where I study computer science and chemical engineering.
-                    Outside of class, I've worked several semesters as a TA in the Computer Science department for classes taught primarily in Python.
+                    I recently graduated from Bucknell with a B.S in Computer Science and Chemical engineering.
+                    Outside of class, I worked several semesters as a TA in the Computer Science department for classes taught primarily in Python.
         </p>
       </section>
       <section>
